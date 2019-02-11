e4c3289fc83983598a15a313a77d137787e9169a
Nirmala <nirmala.talari@gmail.com>
2019-02-11 13:16:59 -0500

    TestingPersonClassSubmission

--- src/main/java/com/zipcodewilmington/person/Person.java
+++ src/main/java/com/zipcodewilmington/person/Person.java
@@ -7,29 +7,45 @@ public class Person {
     private String name;
     private int age;
 
-    public Person() {
+    public Person()
+    {
+        this.name = "";
+        this.age = Integer.MAX_VALUE;
     }
 
-    public Person(int age) {
+    public Person(int age)
+    {
+        setAge(age);
     }
 
-    public Person(String name) {
+    public Person(String name)
+    {
+        setName(name);
     }
 
-    public Person(String name, int age) {
+    public Person(String name, int age)
+    {
+        setName(name);
+        setAge(age);
     }
 
-    public void setName(String name) {
+    public void setName(String name)
+    {
+        this.name = name;
     }
 
-    public void setAge(int age) {
+    public void setAge(int age)
+    {
+        this.age = age;
     }
 
-    public String getName() {
-        return null;
+    public String getName()
+    {
+        return this.name;
     }
 
-    public Integer getAge() {
-        return null;
+    public Integer getAge()
+    {
+        return this.age;
     }
 }
