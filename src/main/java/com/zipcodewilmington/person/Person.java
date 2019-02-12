c8c6b452d4b4e31e0c7039b611cb00bf9d2a4cc8
Nirmala <nirmala.talari@gmail.com>
2019-02-12 16:55:41 -0500

    added 5 fields and testcases

--- src/main/java/com/zipcodewilmington/person/Person.java
+++ src/main/java/com/zipcodewilmington/person/Person.java
@@ -4,48 +4,114 @@ package com.zipcodewilmington.person;
  * Created by leon on 2/12/18.
  */
 public class Person {
+    //instance variables
     private String name;
     private int age;
+    private char gender;
+    private String school;
+    private double height;
+    private boolean isGraduate;
+    private boolean isWorking;
 
-    public Person()
-    {
-        this.name = "";
-        this.age = Integer.MAX_VALUE;
+    //constructors
+    public Person() {
+        this.name = "Leon";
+        this.age = 5;
+        this.gender = 'F';
+        this.school = "Temple";
+        this.height = 5.8;
+        this.isGraduate = true;
+        this.isWorking = false;
     }
 
-    public Person(int age)
-    {
+    public Person(int age) {
         setAge(age);
     }
 
-    public Person(String name)
-    {
+    public Person(String name, String school) {
         setName(name);
+        setSchool(school);
     }
 
-    public Person(String name, int age)
-    {
+    public Person(double height) {
+        setHeight(height);
+    }
+
+    public Person(boolean isGraduate, boolean isWorking) {
+        setGrad(isGraduate);
+        setGrad(isWorking);
+    }
+
+    public Person(char gender) {
+        setGender(gender);
+    }
+
+    public Person(String name, int age, boolean isWorking, boolean isGraduate, double height, char gender, String school) {
         setName(name);
         setAge(age);
+        setHeight(height);
+        setSchool(school);
+        setGender(gender);
+        setGrad(isGraduate);
+        setWork(isWorking);
     }
 
-    public void setName(String name)
-    {
+    // set methods
+    private void setGender(char gender) {
+        this.gender = gender;
+    }
+
+    private void setHeight(double height) {
+        this.height = height;
+    }
+
+    private void setGrad(boolean isGraduate) {
+        this.isGraduate = isGraduate;
+    }
+
+    private void setWork(boolean isWorking) {
+        this.isWorking = isWorking;
+    }
+
+    public void setSchool(String school) {
+        this.school = school;
+    }
+
+    public void setName(String name) {
         this.name = name;
     }
 
-    public void setAge(int age)
-    {
+    public void setAge(int age) {
         this.age = age;
     }
 
-    public String getName()
-    {
+    // get methods
+    public char getGender() {
+        return this.gender;
+    }
+
+    public String getSchool() {
+        return this.school;
+    }
+
+    public boolean getWork() {
+        return this.isWorking;
+    }
+
+    public boolean getGrad() {
+        return this.isGraduate;
+    }
+
+    public double getHeight() {
+        return this.height;
+    }
+
+    public String getName() {
         return this.name;
     }
 
-    public Integer getAge()
-    {
+    public Integer getAge() {
         return this.age;
     }
+
 }
--- src/test/java/com/zipcodewilmington/person/TestPerson.java
+++ src/test/java/com/zipcodewilmington/person/TestPerson.java
@@ -2,6 +2,7 @@ package com.zipcodewilmington.person;
 
 import org.junit.Assert;
 import org.junit.Test;
+import sun.misc.ASCIICaseInsensitiveComparator;
 
 /**
  * Created by leon on 2/12/18.
@@ -10,8 +11,13 @@ public class TestPerson {
     @Test
     public void testDefaultConstructor() {
         // Given
-        String expectedName = "";
-        Integer expectedAge = Integer.MAX_VALUE;
+        String expectedName = "Leon";
+        Integer expectedAge = 5;
+        char expectedgender = 'F';
+        String expectedschool = "Temple";
+        double expectedheight = 5.8;
+        boolean expectedisGraduate = true;
+        boolean expectedisWorking = true;
 
         // When
         Person person = new Person();
@@ -19,53 +25,79 @@ public class TestPerson {
         // Then
         String actualName = person.getName();
         Integer actualAge = person.getAge();
+        double actualHeight = person.getHeight();
+        boolean actualGraduate = person.getGrad();
+        boolean actualWorking = person.getGrad();
+        String actualSchool = person.getSchool();
+        char actualGender = person.getGender();
 
         Assert.assertEquals(expectedName, actualName);
         Assert.assertEquals(expectedAge, actualAge);
+        Assert.assertEquals(expectedheight, actualHeight, 0);
+        Assert.assertEquals(expectedgender, actualGender);
+        Assert.assertEquals(expectedisWorking, actualWorking);
+        Assert.assertEquals(expectedisGraduate, actualGraduate);
+        Assert.assertEquals(expectedschool, actualSchool);
     }
 
     @Test
     public void testConstructorWithName() {
         // Given
-        String expected = "Leon";
+        String expectedName = "Leon";
 
         // When
-        Person person = new Person(expected);
+        Person person = new Person();
 
         // Then
-        String actual = person.getName();
-        Assert.assertEquals(expected, actual);
+        String actualName = person.getName();
+        Assert.assertEquals(expectedName, actualName);
     }
 
     @Test
     public void testConstructorWithAge() {
         // Given
-        Integer expected = 5;
+        Integer expectedAge = 5;
 
         // When
-        Person person = new Person(expected);
+        Person person = new Person();
 
         // Then
-        Integer actual = person.getAge();
-        Assert.assertEquals(expected, actual);
+        Integer actualAge = person.getAge();
+        Assert.assertEquals(expectedAge, actualAge);
     }
 
 
     @Test
-    public void testConstructorWithNameAndAge() {
+    public void testConstructorWithNameAndSchool() {
         // Given
-        Integer expectedAge = 5;
+        String expectedSchool = "Temple";
         String expectedName = "Leon";
 
         // When
-        Person person = new Person(expectedName, expectedAge);
+        Person person = new Person(expectedName, expectedSchool);
 
         // Then
-        Integer actualAge = person.getAge();
+        String actualSchool = person.getSchool();
         String actualName = person.getName();
 
-        Assert.assertEquals(expectedAge, actualAge);
         Assert.assertEquals(expectedName, actualName);
+        Assert.assertEquals(expectedSchool, actualSchool);
+    }
+
+    @Test
+    public void testConstructorWithGradWork() {
+        //Given
+        boolean expectedIsWork = true;
+        boolean expectedIsGrad = false;
+        //When
+        Person obj = new Person(expectedIsGrad, expectedIsWork);
+
+        //Then
+        boolean actualIsWork = obj.getWork();
+        boolean actualIsGrad = obj.getGrad();
+
+        Assert.assertEquals(expectedIsGrad, actualIsGrad);
+        Assert.assertEquals(expectedIsWork, actualIsWork);
     }
 
     @Test
@@ -94,5 +126,18 @@ public class TestPerson {
         // Then
         Integer actual = person.getAge();
         Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testSetSchool() {
+        // Given
+        Person person = new Person();
+        String expectedSchool = "Temple";
+
+        // When
+        String actual = person.getSchool();
+
+        // Then
+        Assert.assertEquals(expectedSchool, actual);
     }
 }
